b77801494a0567de3302c0cd292bb4ef2122094e
Edward Snodgrass <edcrates@gmail.com>
2014-06-03 13:42:22 -0500

    Added ReviewController, ng-submit directive

--- js/app.js
+++ js/app.js
@@ -12,6 +12,14 @@
         };
     });
 
+    app.controller('ReviewController', function(){
+        this.review = {};
+        this.addReview = function(product){
+            product.reviews.push(this.review);
+            this.review = {};
+        }
+    });
+
     app.controller('StoreController', function(){
         this.products = gems;
     });
